b8ca7cde13fe3ffefea1b2f90bb141100353f9dc
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-03-12 21:40:18 +0800

    🎉 Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue3-Vditor
+
+Vue Composition API + Vditor + TypeScript Minimal Example
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="vditor" />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Vditor from 'vditor';
+import 'vditor/dist/index.css';
+
+const vditor = ref<Vditor | null>(null);
+
+onMounted(() => {
+  vditor.value = new Vditor('vditor', {
+    after: () => {
+      // vditor.value is a instance of Vditor now and thus can be safely used here
+      vditor.value!.setValue('Vue Composition API + Vditor + TypeScript Minimal Example');
+    },
+  });
+});
+</script>
+
+<style></style>
